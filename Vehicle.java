93de6fc7787a8d9e2ae55c798ce7ad9d517f6204
Lauren Gallagher <109532911+laurengall@users.noreply.github.com>
2023-04-13 16:38:52 -0400

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,19 @@
+class Vehicle {
+  private String make; 
+  private String model; 
+  private int year; 
+  private int numWheels; 
+
+  public Vehicle(){
+    this.make = make; 
+    this.model = model;
+    this.year = year;
+    this.numWheels = numWheels;
+  }
+  public Vehicle(String ma, String mo, int y, int nW){
+    make = ma; 
+    model = mo;
+    year = y; 
+    numWheels = nW; 
+  }
+}
