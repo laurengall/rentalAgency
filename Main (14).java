289cd806ada77266e0594f5580009075b350f80d
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-04-13 16:49:11 -0400

    commit

--- Main (14).java
+++ Main (14).java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
-   
+    Vehicle v001 = new Vehicle("Ford", "Explorer", 2020, 4, "black");
+    Vehicle v002 = new Motorcycle();
+    Vehicle v003 = new Car();
+    Vehicle v004 = new Truck();
+
   }
 }
--- Vehicle.java
+++ Vehicle.java
@@ -1,19 +1,23 @@
-class Vehicle {
-  private String make; 
-  private String model; 
-  private int year; 
-  private int numWheels; 
+public class Vehicle {
+  private String make;
+  private String model;
+  private int year;
+  private int numWheels;
+  private String color;
 
-  public Vehicle(){
-    this.make = make; 
+  public Vehicle() {
+    this.make = make;
     this.model = model;
     this.year = year;
     this.numWheels = numWheels;
+    this.color = color;
   }
-  public Vehicle(String ma, String mo, int y, int nW){
-    make = ma; 
+
+  public Vehicle(String ma, String mo, int y, int nW, String c) {
+    make = ma;
     model = mo;
-    year = y; 
-    numWheels = nW; 
+    year = y;
+    numWheels = nW;
+    color = c;
   }
 }
