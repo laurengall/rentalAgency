ab721b6137d5f9e527baa25f60c23ba6e80da4db
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-04-14 16:06:42 -0400

    commit
    Getters and setters are in Vehicle.

--- Vehicle.java
+++ Vehicle.java
@@ -20,4 +20,44 @@ public class Vehicle {
     numWheels = nW;
     color = c;
   }
+
+  public String getMake() {
+    return make;
+  }
+
+  public void setMake(String make) {
+    this.make = make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public int getNumWheels() {
+    return numWheels;
+  }
+
+  public void setNumWheels(int numWheels) {
+    this.numWheels = numWheels;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
 }
