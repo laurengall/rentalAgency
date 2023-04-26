7e0f2d7085c0fc1ba10acb5082c78275e1a8efb0
Lauren Gallagher <lauren.21.gallagher@gmail.com>
2023-04-26 18:54:49 -0400

    commit

--- Main (14).java
+++ Main (14).java
@@ -7,40 +7,49 @@ class Main {
     // Car v001 = new Car("Ford", "Explorer", 2020, 4, "Black", 50.0, 001);
     ArrayList<Vehicle> list = new ArrayList<Vehicle>();
     Scanner input = new Scanner(System.in);
+    boolean running = true;
     System.out.println("Bob's Rental Agency Admin Console. Type a number from the menu to get started.");
-    System.out.println("1. Find Vehicle");
-    System.out.println("2. Calculate Rental Fees");
-    System.out.println("3. Edit Vehicle Info");
-    System.out.println("4. Add Customer");
-    System.out.println("5. View Customer Info");
-    System.out.println("6. Edit Customer Info");
-    System.out.println("7. Add Vehicle");
-    int cmd = input.nextInt();
-    switch (cmd) {
-      case 1:
-        findVehicle(input, list);
-        break;
-      case 2:
-        calcFees();
-        break;
-      case 3:
-        editVehicle();
-        break;
-      case 4:
-        addCust();
-        break;
-      case 5:
-        viewCust();
-        break;
-      case 6:
-        editCust();
-        break;
-      case 7:
-        addVehicle(input, list);
-        break;
-      default:
-        break;
+    while (running) {
+      System.out.println("1. Find Vehicle");
+      System.out.println("2. Calculate Rental Fees");
+      System.out.println("3. Edit Vehicle Info");
+      System.out.println("4. Add Customer");
+      System.out.println("5. View Customer Info");
+      System.out.println("6. Edit Customer Info");
+      System.out.println("7. Add Vehicle");
+      System.out.println("8. Exit");
+      int cmd = input.nextInt();
+      switch (cmd) {
+        case 1:
+          findVehicle(input, list);
+          break;
+        case 2:
+          calcFees();
+          break;
+        case 3:
+          editVehicle();
+          break;
+        case 4:
+          addCust();
+          break;
+        case 5:
+          viewCust();
+          break;
+        case 6:
+          editCust();
+          break;
+        case 7:
+          Vehicle v = new Vehicle();
+          addVehicle(input, list);
+          break;
+        case 8:
+          running = false;
+          break;
+        default:
+          break;
+      }
     }
+
   }
 
   public static void findVehicle(Scanner input, ArrayList<Vehicle> list) {
@@ -81,15 +90,15 @@ class Main {
     int num = input.nextInt();
     if (num == 1) {
       Car c = new Car();
-      getInfo(input, c, list);
+      getInfo(c, list);
       list.add(c);
     } else if (num == 2) {
       Truck t = new Truck();
-      getInfo(input, t, list);
+      getInfo(t, list);
       list.add(t);
     } else if (num == 3) {
       Motorcycle m = new Motorcycle();
-      getInfo(input, m, list);
+      getInfo(m, list);
       list.add(m);
     } else {
       System.out.println("What type of vehicle will you be adding?");
@@ -98,24 +107,35 @@ class Main {
     }
   }
 
-  public static void getInfo(Scanner input, Vehicle v, ArrayList<Vehicle> list) {
+  public static void getInfo(Vehicle v, ArrayList<Vehicle> list) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter make name: ");
+    String mak = "";
+    mak = input.nextLine();
+    v.setMake(mak);
     System.out.println("Enter model name: ");
-    String mod = input.nextLine();
+    String mod = "";
+    mod = input.nextLine();
     v.setModel(mod);
     System.out.println("Enter year of manufacture: ");
-    int yea = input.nextInt();
+    int yea = 0;
+    yea = input.nextInt();
     v.setYear(yea);
     System.out.println("Enter year of number of wheels: ");
-    int nW = input.nextInt();
+    int nW = 0;
+    nW = input.nextInt();
     v.setNumWheels(nW);
     System.out.println("Enter color name: ");
-    String col = input.nextLine();
+    String col = "";
+    col = input.nextLine();
     v.setColor(col);
     System.out.println("Enter price per day: ");
-    double price = input.nextDouble();
+    double price = 0.0;
+    price = input.nextDouble();
     v.setPpd(price);
     System.out.println("Enter vin number: ");
-    int vinNum = input.nextInt();
+    int vinNum = 0;
+    vinNum = input.nextInt();
     v.setVin(vinNum);
     list.add(v);
   }
